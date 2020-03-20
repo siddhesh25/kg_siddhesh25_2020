8840bd84220f63082c628f0b10578cd9aec9dcca
siddhesh25 <siddheshyadav1@gmail.com>
2020-03-20 04:26:40 -0400

    Kargo online assessment

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import sys
+#We need to whether a one-to-one character mapping exists from one string, s1, to another string,s2.
+#Checking if both the strings are defined frm input argument. If not, throw an error.
+if(len(sys.argv) != 3):
+    sys.exit("Enter strings s1 and s2 with space in between");
+if(len(sys.argv[1]) != len(sys.argv[2])):
+    sys.exit("Enter strings s1 and s2 of same length as it is assumed that one on one mapping is required")
+s1 = sys.argv[1]                 #Splitting both strings into s1 and s2.
+s2 = sys.argv[2]                 #Splitting both strings into s1 and s2.
+str_dict = dict()
+is_unique = True
+for character in s1:             #Looping through the first string
+    if(not str_dict.get(character, False)):   #Checking if the character exists in the dictionary or not
+        str_dict[character] = character       #If does not exists then enter in the dictionary for further use if required 
+    else:
+        is_unique = False                     #If does not exists then set the flag to false
+        break
+#print the result based on flag
+if is_unique:
+    print("True")
+else:
+    print("False")
+
+
+# In[ ]:
+
+
+
+
